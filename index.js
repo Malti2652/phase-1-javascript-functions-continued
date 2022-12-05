5803592e0b6f973c659b176a45c268101e5d7e1c
Eddington Rowellis <eddingtonibi@gmail.com>
2022-12-05 16:26:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(verb = 'roller-skate'){
+    return `This Saturday, I want to ${verb}!`;
+}
+
+const mondayWork = function (action = 'go to the office') {
+    return`This Monday, I will ${action}.`
+}
+
+function wrapAdjective (phrase="*") {
+    return function(word = "special"){
+        return `You are ${phrase}${word}${phrase}!`
+    }
+}
